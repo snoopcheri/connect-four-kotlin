a6bfbd60871fa7909e10fbfda547a8076df3b796
Sargon <sargon@me.com>
2020-04-07 20:42:56 +0200

    Change random computer player to use move iterator

--- src/main/kotlin/name/sargon/connect4/RandomComputerPlayer.kt
+++ src/main/kotlin/name/sargon/connect4/RandomComputerPlayer.kt
@@ -1,22 +1,15 @@
 package name.sargon.connect4
 
-import kotlin.random.Random
+class RandomComputerPlayer : Player {
 
-class RandomComputerPlayer: Player {
+    private val moveGenerator = SimpleMoveGenerator()
 
     override fun nextMove(board: Board): Square {
-        val moves = board.moves()
-        val nMoves = moves.countOnes()
-        val chosenMoveNr = Random.nextInt(0, nMoves)
-
-        var randomMove = moves
-
-        repeat(chosenMoveNr - 1) {
-            val nextOne = randomMove.nextOne()
-            randomMove = randomMove.cleared(nextOne)
-        }
-
-        return randomMove.nextOne()
+        return moveGenerator.moves(board)
+            .asSequence()
+            .toList()
+            .shuffled()
+            .first()
     }
 
 }
